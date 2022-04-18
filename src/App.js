e2ca5b5728a4fe18e75688b3f2b1c82148af6342
aafrinabid <mohdaafrin@gmail.com>
2022-04-18 12:00:47 +0530

    food app v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import './App.css';
+import Header from './components/layout/Header';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+   
+  );
+}
+
+export default App;
