ea3b8115a278ceb88c522c04110fb6399b91e822
aafrinabid <mohdaafrin@gmail.com>
2022-04-26 20:14:15 +0530

    database added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,51 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
 function AvailableMeals() {
-     const mealsList=DUMMY_MEALS.map(meal=>(
+  const [meals,setMeals]=useState([]);
+  const [isLoading,setIsLoading]=useState(true);
+  const [httpError,setHttpError]=useState(null)
+
+  useEffect(()=>{
+  const fetchmeals=async()=>{
+    setIsLoading(true)
+    const response=await fetch('https://react-http-b5dbb-default-rtdb.firebaseio.com/meals.json')
+    const data=await response.json();
+    const loadedMeals=[];
+
+    for(const key in data){
+      loadedMeals.push({
+         id:key,
+         name:data[key].name,
+         description:[key].description,
+         price:data[key].price,  
+
+      });
+    }
+setMeals(loadedMeals)
+setIsLoading(false); 
+
+  };
+
+    fetchmeals().catch((error)=>{
+      setIsLoading(false);
+      setHttpError(error.message)
+    });
+
+},[]);
+   
+   if(httpError){
+     return(
+     <section className={classes.MealsError}>
+       <p>{httpError}</p>
+     </section>
+
+     )
+   }
+
+    const mealsList=meals.map(meal=>(
 <MealItem 
 key={meal.id}
 id={meal.id}
@@ -43,7 +58,9 @@ price={meal.price}
     <section className={classes.meals}>
         <Card>
         <ul>
-      {mealsList}
+          
+          {isLoading &&<section className="isloading">loading...</section>}
+      {!isLoading && mealsList}
         </ul>
         </Card>
     </section>
